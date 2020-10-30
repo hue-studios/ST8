2254a8f0f48de9abedb8d59c1625d22c3f5dde3d
pvenableh <peter@huestudios.com>
2020-10-30 13:20:28 -0400

    Updated config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -43,5 +43,16 @@ module.exports = {
     extend: {}
   },
   variants: {},
-  plugins: []
+  plugins: [],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js'
+    ]
+  }
 }
