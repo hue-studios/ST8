c25d181bad4f3b2d28b7fe75b8e72023f11b003f
pvenableh <peter@huestudios.com>
2021-02-24 19:44:03 -0500

    Updated config.

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,8 +2,8 @@ export default {
   // ssr: 'true',
   // static: 'false',
   server: {
-    // port: 8080, // default: 3000
-    // host: '0.0.0.0' // default: localhost
+    port: 8080, // default: 3000
+    host: '0.0.0.0' // default: localhost
   },
   /*
    ** Headers of the page
