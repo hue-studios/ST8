6ffc21e53bf2bf285427a619321d82ae938f8a32
Peter Hoffman <peter@huestudios.com>
2025-03-01 12:22:40 -0500

    Updated home request.

--- pages/index.vue
+++ pages/index.vue
@@ -228,6 +228,7 @@
       <div id="cropped-overlay" class="absolute"></div>
     </div>
     <div
+      v-if="homeData.featured_resource"
       id="home__resource"
       class="w-full flex justify-center items-center flex-row flex-wrap relative shadow-lg home__section"
     >
@@ -238,18 +239,8 @@
           >Featured </span
         >Resource
       </h2>
-      <!-- <div class="w-full pl-3 pr-3 mb-2 lg:pl-5 lg:pr-5 home__subtitle">
-
-      </div> -->
-      <!-- <now-resource-card-data :item="resource" class="" /> -->
 
       <now-resource-card-data :item="homeData.featured_resource" class="" />
-      <!-- <nuxt-link
-        to="/regional-news"
-        class="green w-full text-center pt-8 tracking-widest uppercase link-font"
-        >View All {{ newsMeta.meta.status_count.published }} Articles
-        <link-icon class="ml-2"></link-icon
-      ></nuxt-link> -->
     </div>
     <iframe
       src="https://betterconnection.org/banner"
@@ -528,7 +519,7 @@ export default {
       // eventsReq,
       programsReq,
       organizationReq,
-      resourceReq,
+      // resourceReq,
       homeReq,
     ] = await Promise.all([
       $axios.get(process.env.apiUrl + '/items/partners?fields=title,category'),
@@ -542,9 +533,9 @@ export default {
           '/items/programs?fields=title,what_is_it,url,featured,counties.county_id.title,images.file_id.private_hash,initiatives.initiative_id.title,partners.partner_id.title&filter[featured]=1&meta=*'
       ),
       $axios.$get('/items/organization?single=1&fields=mission_statement'),
-      $axios.$get('/items/resources/80?fields=*.*'),
+      // $axios.$get('/items/resources/80?fields=*.*'),
       $axios.$get(
-        '/items/home?fields=introduction,featured_resource.*&single=1'
+        '/items/home?fields=introduction,featured_resource.*,featured_resource.file.private_hash&single=1'
       ),
     ])
     return {
@@ -555,7 +546,7 @@ export default {
       programs: programsReq.data.data,
       programsMeta: programsReq.data,
       organization: organizationReq.data,
-      resource: resourceReq.data,
+      // resource: resourceReq.data,
       homeData: homeReq.data,
     }
   },
